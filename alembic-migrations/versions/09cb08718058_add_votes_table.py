ade628a283488c410831cd49e2acf404c0e8c60e
jdelarua <jesusdelarua@yahoo.com>
2022-01-25 09:15:50 -0500

    Deprecated Alembic revision 09cb08718058

--- alembic-migrations/versions/09cb08718058_add_votes_table.py
+++ alembic-migrations/versions/09cb08718058_add_votes_table.py
@@ -17,30 +17,30 @@ depends_on = None
 
 
 def upgrade():
-     #Create Table votes
-     op.create_table('votes',
-                    sa.Column('user_id', sa.Integer(), nullable=False),
-                    sa.Column('post_id', sa.Integer(), nullable=False)
-                    )
-     #Add  composite PK
-     op.create_primary_key('votes_comp_pkey',
-                           'votes',
-                           ['user_id','post_id']    
-                          )   
-     #Add user_id FK
-     op.create_foreign_key('votes_user_id_FK', 
-                          source_table='votes', 
-                          referent_table='users',
-                          local_cols=['user_id'], 
-                          remote_cols=['id'],
-                          ondelete="CASCADE")                                      
-     #Add post_id FK
-     op.create_foreign_key('votes_post_id_FK', 
-                          source_table='votes', 
-                          referent_table='posts',
-                          local_cols=['post_id'], 
-                          remote_cols=['id'],
-                          ondelete="CASCADE")                                      
+     # #Create Table votes
+     # op.create_table('votes',
+     #                sa.Column('user_id', sa.Integer(), nullable=False),
+     #                sa.Column('post_id', sa.Integer(), nullable=False)
+     #                )
+     # #Add  composite PK
+     # op.create_primary_key('votes_comp_pkey',
+     #                       'votes',
+     #                       ['user_id','post_id']    
+     #                      )   
+     # #Add user_id FK
+     # op.create_foreign_key('votes_user_id_FK', 
+     #                      source_table='votes', 
+     #                      referent_table='users',
+     #                      local_cols=['user_id'], 
+     #                      remote_cols=['id'],
+     #                      ondelete="CASCADE")                                      
+     # #Add post_id FK
+     # op.create_foreign_key('votes_post_id_FK', 
+     #                      source_table='votes', 
+     #                      referent_table='posts',
+     #                      local_cols=['post_id'], 
+     #                      remote_cols=['id'],
+     #                      ondelete="CASCADE")                                      
      pass
 
 def downgrade():
